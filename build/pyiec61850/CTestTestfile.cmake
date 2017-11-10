dffe21930323f2b845bb7717cf7b1c28ca873101
Steffen Vogel <post@steffenvogel.de>
2017-11-10 14:36:49 +0100

    fix version incompatability in Swig

---
+++ build/pyiec61850/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/stv0g/workspace/rwth/villas/node/thirdparty/libiec61850/pyiec61850
+# Build directory: /Users/stv0g/workspace/rwth/villas/node/thirdparty/libiec61850/build/pyiec61850
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_pyiec61850 "/opt/local/bin/python3.6" "/Users/stv0g/workspace/rwth/villas/node/thirdparty/libiec61850/pyiec61850/test_pyiec61850.py")
