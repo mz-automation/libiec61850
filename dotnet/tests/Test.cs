9b7a2f086e2bc578ecfd9ce68c7b7a17a5ebe98e
Michael Zillgith <michael.zillgith@mz-automation.de>
2019-06-01 18:09:33 +0200

    - .NET: added missing ignore paramter for test case

--- dotnet/tests/Test.cs
+++ dotnet/tests/Test.cs
@@ -425,7 +425,7 @@ namespace tests
 		}
 
 		[Test()]
-        [Ignore()]
+        [Ignore("has to be fixed")]
 		public void ControlHandler()
 		{
 			IedModel iedModel = ConfigFileParser.CreateModelFromConfigFile ("../../model.cfg");
