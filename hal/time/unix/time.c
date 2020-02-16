d0da45be415970f06f3346e95a0a50996990a7be
Michael Zillgith <michael.zillgith@mz-automation.de>
2020-02-16 15:17:25 +0100

    - HAL: added new functions to handle system time with nanosecond resolution

--- hal/time/unix/time.c
+++ hal/time/unix/time.c
@@ -39,7 +39,7 @@ Hal_getTimeInMs()
 
 #include <sys/time.h>
 
-uint64_t
+msSinceEpoch
 Hal_getTimeInMs()
 {
     struct timeval now;
@@ -48,6 +48,35 @@ Hal_getTimeInMs()
 
     return ((uint64_t) now.tv_sec * 1000LL) + (now.tv_usec / 1000);
 }
+
+nsSinceEpoch
+Hal_getTimeInNs()
+{
+    struct timespec now;
+
+    clock_gettime(CLOCK_REALTIME, &now);
+
+    nsSinceEpoch nsTime = now.tv_sec * 1000000000UL;
+    nsTime += now.tv_nsec;
+
+    return nsTime;
+}
+
+bool
+Hal_setTimeInNs(nsSinceEpoch nsTime)
+{
+    struct timespec tv;
+
+    tv.tv_sec = nsTime / 1000000000UL;
+    tv.tv_nsec = nsTime % 1000000000UL;
+
+    if (clock_settime(CLOCK_REALTIME, &tv) < 0) {
+        return false;
+    }
+
+    return true;
+}
+
 
 #endif
 
