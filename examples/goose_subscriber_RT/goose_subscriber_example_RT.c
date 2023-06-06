8187e434eeb237f65f12b402c50baf143805aba9
Ben Haines <bphaines@gmail.com>
2023-06-05 22:37:16 -0700

    Add RT examples

---
+++ examples/goose_subscriber_RT/goose_subscriber_example_RT.c
@@ -0,0 +1,98 @@
+/*
+ * goose_subscriber_example.c
+ *
+ * This is an example for a standalone GOOSE subscriber
+ *
+ * Has to be started as root in Linux.
+ *
+ * BPH - 11-8-21
+ * Adapted from original goose publlisher example to implement realtime threading and priorities 
+ *
+ */
+
+#include "goose_receiver.h"
+#include "goose_subscriber.h"
+#include "hal_thread.h"
+#include "linked_list.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+
+static int running = 1;
+
+static void
+sigint_handler(int signalId)
+{
+    running = 0;
+}
+
+static void
+gooseListener(GooseSubscriber subscriber, void* parameter)
+{
+    printf("GOOSE event:\n");
+    printf("  stNum: %u sqNum: %u\n", GooseSubscriber_getStNum(subscriber),
+            GooseSubscriber_getSqNum(subscriber));
+    printf("  timeToLive: %u\n", GooseSubscriber_getTimeAllowedToLive(subscriber));
+
+    uint64_t timestamp = GooseSubscriber_getTimestamp(subscriber);
+
+    printf("  timestamp: %u.%u\n", (uint32_t) (timestamp / 1000), (uint32_t) (timestamp % 1000));
+    printf("  message is %s\n", GooseSubscriber_isValid(subscriber) ? "valid" : "INVALID");
+
+    MmsValue* values = GooseSubscriber_getDataSetValues(subscriber);
+
+    char buffer[1024];
+
+    MmsValue_printToBuffer(values, buffer, 1024);
+
+    printf("  allData: %s\n", buffer);
+}
+
+int
+main(int argc, char** argv)
+{
+	// BPH Notes: Single receiver on interface with potentially multiple subscribers.
+    GooseReceiver receiver = GooseReceiver_create();  //BPH Notes: Mallocs Receiver Struct and Eth Buffer
+
+    if (argc > 1) {
+        printf("Set interface id: %s\n", argv[1]);
+        GooseReceiver_setInterfaceId(receiver, argv[1]);
+    }
+    else {
+        printf("Using interface eth0\n");
+        GooseReceiver_setInterfaceId(receiver, "eth0");
+    }
+
+    GooseSubscriber subscriber = GooseSubscriber_create("simpleIOGenericIO/LLN0$GO$gcbAnalogValues", NULL);
+
+    uint8_t dstMac[6] = {0x01,0x0c,0xcd,0x01,0x00,0x01};
+    GooseSubscriber_setDstMac(subscriber, dstMac);
+    GooseSubscriber_setAppId(subscriber, 1000);
+
+	// BPH Notes: Attach listner function to subscriber struct, called when goose received that matches
+    GooseSubscriber_setListener(subscriber, gooseListener, NULL);
+
+	// BPH Notes: add subscriber struct to receiver list
+    GooseReceiver_addSubscriber(receiver, subscriber);
+
+	// BPH Notes: call when all subscribers added
+    GooseReceiver_start_RT(receiver);  // Create receive thread with RT  priority 
+
+    if (GooseReceiver_isRunning(receiver)) {
+        signal(SIGINT, sigint_handler);
+
+        while (running) {
+            Thread_sleep(100);
+        }
+    }
+    else {
+        printf("Failed to start GOOSE subscriber. Reason can be that the Ethernet interface doesn't exist or root permission are required.\n");
+    }
+
+    GooseReceiver_stop(receiver);
+
+    GooseReceiver_destroy(receiver);
+
+    return 0;
+}
