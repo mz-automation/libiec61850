7604eec0a99dbb6ff96411424449c9aaeb32619e
Michael Zillgith <michael.zillgith@mz-automation.de>
2026-06-22 18:23:12 +0100

    - removed unused code from buffer_chain.c/.h

--- src/common/buffer_chain.c
+++ src/common/buffer_chain.c
@@ -34,25 +34,3 @@ BufferChain_init(BufferChain self, int length, int partLength, BufferChain nextP
     self->nextPart = nextPart;
     self->buffer = buffer;
 }
-
-void
-MemoryArea_initialize(MemoryArea* self, uint8_t* memory, int size)
-{
-    self->memory = memory;
-    self->size = size;
-    self->currentPos = 0;
-}
-
-uint8_t*
-MemoryArea_getNextBlock(MemoryArea* self, int size)
-{
-    if ((self->size - self->currentPos) >= size) {
-        uint8_t* newBlock = self->memory + self->currentPos;
-
-        self->currentPos += size;
-        return newBlock;
-    }
-    else
-        return NULL;
-
-}
--- src/common/inc/buffer_chain.h
+++ src/common/inc/buffer_chain.h
@@ -39,48 +39,4 @@ struct sBufferChain {
 LIB61850_INTERNAL void
 BufferChain_init(BufferChain self, int length, int partLength, BufferChain nextPart, uint8_t* buffer);
 
-typedef struct {
-    uint8_t* memory;
-    int currentPos;
-    int size;
-} MemoryArea;
-
-LIB61850_INTERNAL void
-MemoryArea_initialize(MemoryArea* self, uint8_t* memory, int size);
-
-LIB61850_INTERNAL uint8_t*
-MemoryArea_getNextBlock(MemoryArea* self, int size);
-
-#if 0
-
-typedef struct sMemoryPool* MemoryPool;
-
-typedef struct sMemoryChunk MemoryChunk;
-
-struct sMemoryPool {
-    uint8_t* memory;
-    int size;
-    MemoryChunk firstChunk;
-};
-
-struct sMemoryChunk {
-    MemoryChunk previous;
-    MemoryChunk next;
-    uint8_t* data;
-    uint8_t free;
-    int size;
-};
-
-MemoryPool
-MemoryPool_create(uint8_t* memoryAddress, int size);
-
-void
-MemoryPool_destroy(MemoryPool self);
-
-MemoryChunk
-MemoryPool_allocateChunk(MemoryPool self, int size);
-MemoryPool_freeChunk(MemoryPool self, MemoryChunk chunk);
-
-#endif
-
 #endif /* BUFFER_CHAIN_H_ */
